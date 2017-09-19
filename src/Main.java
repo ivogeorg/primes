7061ec56dbbad2cb957171ff1afd8c2a2977a5f6
Ivo Georgiev <ivogeorg@gmail.com>
2017-09-18 19:31:55 -0600

    added some comments

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // limit
         int number = Integer.parseInt(args[0]);
 
         boolean[] primes = new boolean[number + 1];
+
+        // all numbers are prime...
         for (int i = 2; i < number + 1; i++) primes[i] = true;
 
+        // except multiples
         for (int i = 2; i < number + 1; i++) {
             for (int j = 2; i * j < number + 1; j++) {
                 primes[i * j] = false;
             }
         }
 
+        // print out
         for (int i = 2; i < number + 1; i++)
             if (primes[i])
                 System.out.print(i + " ");
