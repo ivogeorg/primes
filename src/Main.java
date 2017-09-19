39d2c2e7efc72877c0cdea6cead58f0ee6139b8d
Ivo Georgiev <ivogeorg@gmail.com>
2017-09-18 19:24:36 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// Quick implementation of the Sieve of Eratosthenes
+// All primes up to and including a limit
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = Integer.parseInt(args[0]);
+
+        boolean[] primes = new boolean[number + 1];
+        for (int i = 2; i < number + 1; i++) primes[i] = true;
+
+        for (int i = 2; i < number + 1; i++) {
+            for (int j = 2; i * j < number + 1; j++) {
+                primes[i * j] = false;
+            }
+        }
+
+        for (int i = 2; i < number + 1; i++)
+            if (primes[i])
+                System.out.print(i + " ");
+    }
+}
